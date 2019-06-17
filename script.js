5aff159dab663d378e293a9f63af2b7f19ac117a
Candice <kaye_candy14@yahoo.com>
2019-06-17 18:17:21 +0800

    init

---
+++ script.js
@@ -0,0 +1,293 @@
+/***************************************************************************
+ *  																	   *
+ *																		   *
+ *	     ___           ___           ___           ___           ___       *
+ *	    /  /\         /  /\         /  /\         /  /\         /  /\      *
+ *	   /  /::\       /  /::|       /  /::\       /  /::\       /  /::\     *
+ *	  /  /:/\:\     /  /:|:|      /  /:/\:\     /  /:/\:\     /  /:/\:\    *
+ *	 /  /:/  \:\   /  /:/|:|__   /  /:/  \:\   /  /:/  \:\   /  /::\ \:\   *
+ *	/__/:/ \  \:\ /__/:/ |:| /\ /__/:/ \__\:| /__/:/ \  \:\ /__/:/\:\ \:\  *
+ *	\  \:\  \__\/ \__\/  |:|/:/ \  \:\ /  /:/ \  \:\  \__\/ \  \:\ \:\_\/  *
+ *	 \  \:\           |  |:/:/   \  \:\  /:/   \  \:\        \  \:\ \:\    *
+ *	  \  \:\          |__|::/     \  \:\/:/     \  \:\        \  \:\_\/    *
+ *	   \  \:\         /__/:/       \__\::/       \  \:\        \  \:\      *
+ *	    \__\/         \__\/            ~~         \__\/         \__\/      *
+ * 																		   *
+ *																		   *
+ ***************************************************************************/
+var CNDCE = {};
+
+$(function(){
+	var $containerStartSurvey = $('#cndce-start-survey-container');
+	var $containerBasicDetails = $('#cndce-basic-details-container');
+	var $containerQuestions = $('#cndce-questions-container');
+
+	var $questionsHeader = $('#cndce-questions-header');
+	var $questionDetailsContainer = $('.cndce-question-details-container', $containerQuestions);
+	var $questionDetailsTemplate = $('.cndce-question-details.cndce-template', $containerQuestions);
+
+	var $inputQuestionNum = $('#question-num-input');
+	var $inputResponseTracking = $('#response-tracking-input');
+
+	var $inputFilesDropzone = $('#cndce-files-dropzone');
+
+	var $btnGenerateSurvey = $('#cndce-generate-survey-button');
+	var $btnEditQuestions = $('#cndce-edit-questions-button');
+
+	var $btnsFileUpload = $('.cndce-upload-wrapper .cndce-upload-button');
+
+	var dropzone;
+
+	var questions = [];
+	var files = [];
+
+
+	$('input.timepicker').pickatime();
+	$('input.datepicker').pickadate();
+
+	$('.mdb-select').materialSelect();
+
+
+	function getTemplate($template){
+		return $template
+			.clone(true)
+			.removeClass('cndce-template');
+	}
+
+
+
+	function addFile(file){
+		var $fileList = $('select.cndce-file-list');
+
+
+		files.push(file);
+		file.options = [];
+
+		$fileList.each(function(){
+			var $select = $(this);
+			var $option = $('<option class="cndce-file">' + file.name + '</option>');
+
+			$select.append($option);
+			file.options.push($option);
+
+			updateSelectValue($select);
+
+		});
+
+
+		// Set added file as value to a select
+		// Called when file is uploaded through an individual upload button
+		if(file.$cndceInput){
+			var $select = $('select.cndce-file-list', file.$cndceInput);
+
+			$select.val(file.name);
+		}
+	}
+
+	function removeFile(file){
+		var $fileList = $('select.cndce-file-list');
+
+		var iFile = files.indexOf(file);
+
+
+		for(var i=0; i < file.options.length; i++){
+			if(file.options[i].prop('selected')){
+				file.options[i].parent().val('');
+			}
+			file.options[i].remove();
+		}
+
+		$fileList.each(function(){
+			updateSelectValue($(this));
+		})
+
+		if(iFile >= 0){
+			files.splice(iFile, 1);
+		}
+	}
+
+
+	// Solution for MDB bug -___-"
+	// Selected value becomes empty when updated
+	// Very disappointed. Tsk tsk
+	function updateSelectValue($selectNode){
+		var $wrapper = $selectNode.parents('.cndce-select-wrapper');
+
+		var observer = new MutationObserver(function(mutationList, observer){
+			for(var mutation of mutationList){
+				for(addedNode of mutation.addedNodes){
+					if($(addedNode).is('div.select-wrapper')){
+						$('ul.dropdown-content li.active', addedNode).click();
+
+						// Another bug - label doesn't go back down when value is empty
+						if(!$('select', addedNode).val()){
+							$(addedNode).siblings('label').removeClass('active');
+						}
+					}
+				}
+			}
+
+			observer.disconnect();
+		})
+
+
+		observer.observe($wrapper[0], { childList: true})
+	}
+
+
+
+
+	function updateQuestions(){
+
+
+		// Init Question Header and Details
+		for(var i=0; i < questions.length; i++){
+			// $inputResponseTracking.append('<option class="cndce-option">' + questions[i].name + '</option>');
+
+			var $header = $('<div class="cndce-question-header">' + questions[i].name + '</div>');
+			var $details = initQuestionDetail(questions[i]);
+
+			$header.data('$details', $details);
+
+			$questionsHeader.append($header);
+
+		}
+
+
+
+		// Init Question Select Options
+		var $select = $('select.cndce-questions-list');
+
+		for(var i=0; i < questions.length; i++){
+			$select.append('<option class="cndce-option">' + questions[i].name + '</option>');
+		}
+
+
+		// Select first question
+		$('.cndce-question-header:first-child', $questionsHeader).click();
+	}
+
+	function initQuestionDetail(question){
+		var $details = getTemplate($questionDetailsTemplate);
+
+		var $questionNumContainer = $('.cndce-question-numpad', $details);
+		var $questionNumColTemplate = $('.cndce-question-num-col.cndce-template', $details);
+
+		var numKeys = [1,2,3,4,5,6,7,8,9,'*',0,'#'];
+
+		// Initialize numpad
+		for(var i=0; i < numKeys.length; i++){
+			var $questionNumCol = getTemplate($questionNumColTemplate);
+
+			$('.cndce-num', $questionNumCol).text(numKeys[i]);
+
+			$questionNumContainer.append($questionNumCol);
+
+		}
+
+		$questionDetailsContainer.append($details);
+
+
+		// Initialize select
+		$('.cndce-file-list, .cndce-questions-list', $details).addClass('mdb-select').materialSelect();
+
+		// $('.mdb-select', $details).materialSelect();
+
+
+		return $details;
+	}
+
+
+	function initQuestions(){
+		var nQuestions = $inputQuestionNum.val();
+
+		for(var i=0; i < nQuestions; i++){
+			questions.push({
+				'name': 'Question ' + (i + 1)
+			})
+		}
+
+		updateQuestions();
+	};
+
+	(function initDropZone(){
+
+		dropzone = new Dropzone('.cndce-dropzone', {
+			url: '#',
+			uploadMultiple: true,
+			addRemoveLinks: true
+		});
+
+
+		dropzone.on('addedfile', function(file, response, e, f){
+			addFile(file);
+		})
+
+		dropzone.on('removedfile', function(file){
+			removeFile(file);
+		})
+
+	})();
+
+
+	(function testVars(){
+		CNDCE.questions = questions;
+		CNDCE.dropzone = dropzone;
+		CNDCE.files = files;
+	})();
+
+
+	$btnsFileUpload.siblings('input[type="file"]').change(function(e){
+		if(this.files.length > 0){
+			this.files[0].$cndceInput = $(this).parents('.cndce-upload-wrapper');
+			dropzone.addFile(this.files[0]);
+		}
+
+	})
+
+
+	$btnsFileUpload.click(function(){
+		var $btn = $(this);
+		var $inputFile = $btn.siblings('input[type="file"]');
+
+		$inputFile.click();
+
+	})
+
+	$questionsHeader.on('click', '.cndce-question-header', function(){
+		var $header = $(this);
+
+		$('.active', $questionsHeader).removeClass('active');
+		$header.addClass('active');
+
+		$('.cndce-question-details.active', $questionDetailsContainer).removeClass('active');
+		$header.data('$details').addClass('active');
+	})
+
+	$containerQuestions.on('click', '.cndce-question-num', function(e){
+		$(this).toggleClass('active');
+	})
+
+	$containerQuestions.on('click', '.cndce-question-input', function(e){
+		e.stopPropagation();
+	})
+
+
+	$btnGenerateSurvey.click(function(){
+		$containerStartSurvey.addClass('cndce-collapse');
+		$containerBasicDetails.addClass('cndce-show');
+
+		initQuestions();
+	})
+
+	$btnEditQuestions.click(function(){
+		$containerBasicDetails.removeClass('cndce-show');
+		$containerBasicDetails.addClass('cndce-collapse');
+		$containerQuestions.addClass('cndce-show');
+	})
+
+
+
+
+
+})
